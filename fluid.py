e3858b7e433486850fda7812baf326017e3ccb38
reschirra <remi.schirra@etu.uca.fr>
2024-06-21 17:48:47 +0200

    first commit

---
+++ fluid.py
@@ -0,0 +1,147 @@
+import numpy as np
+from numba import njit
+from numba import int32, float32, boolean    # import the types
+from numba.experimental import jitclass
+
+spec = [
+    ('width', int32),
+    ('height', int32),
+    ('dt', float32),
+    ('diffusion', float32),
+    ('viscosity', float32),
+    ('s', float32[:]),
+    ('density', float32[:]),
+    ('Vx', float32[:]),
+    ('Vy', float32[:]),
+    ('Vx0', float32[:]),
+    ('Vy0', float32[:])
+]
+
+class Fluid:
+    def __init__(self, width, height, diffusion, viscosity, dt):
+        self.width = width
+        self.height = height
+        self.dt = dt
+        self.diffusion = diffusion
+        self.viscosity = viscosity
+
+        self.s = np.zeros((width, height))
+        self.density = np.zeros((width, height))
+
+        self.Vx = np.zeros((width, height))
+        self.Vy = np.zeros((width, height))
+        
+        self.Vx0 = np.zeros((width, height))
+        self.Vy0 = np.zeros((width, height))
+        self.obstacles = np.zeros((width, height), dtype=bool)
+
+    def step(self):
+        self.diffuse(1, self.Vx0, self.Vx, self.viscosity)
+        self.diffuse(2, self.Vy0, self.Vy, self.viscosity)
+
+        self.project(self.Vx0, self.Vy0, self.Vx, self.Vy)
+
+        self.advect(1, self.Vx, self.Vx0, self.Vx0, self.Vy0)
+        self.advect(2, self.Vy, self.Vy0, self.Vx0, self.Vy0)
+
+        self.project(self.Vx, self.Vy, self.Vx0, self.Vy0)
+
+        self.diffuse(0, self.s, self.density, self.diffusion)
+        self.advect(0, self.density, self.s, self.Vx, self.Vy)
+
+    def add_density(self, x, y, amount):
+        self.density[x, y] += amount
+
+    def add_velocity(self, x, y, amountX, amountY):
+        self.Vx[x, y] += amountX
+        self.Vy[x, y] += amountY
+    
+    def fade_velocity(self, x, y):
+        self.Vx[x, y] -= 7
+        self.Vy[x, y] -= 7
+        self.Vx[x, y] = max(self.Vx[x, y], 0)
+        self.Vy[x, y] = max(self.Vy[x, y], 0)
+
+    def fade_density(self, x, y):
+        self.density[x, y] -= 7
+        self.density[x, y] = max(self.density[x, y], 0)
+
+    def diffuse(self, b, x, x0, diff):
+        a = self.dt * diff * self.width * self.height
+        self.lin_solve(b, x, x0, a, 1 + 4 * a)
+
+    def lin_solve(self, b, x, x0, a, c):
+        c_recip = 1.0 / c
+        for k in range(4):
+            for i in range(1, self.width - 1):
+                for j in range(1, self.height - 1):
+                    if not self.obstacles[i, j]:
+                        x[i, j] = (x0[i, j] + a * (x[i+1, j] + x[i-1, j] + x[i, j+1] + x[i, j-1])) * c_recip
+            self.set_bnd(b, x)
+
+    def project(self, velocX, velocY, p, div):
+        for i in range(1, self.width - 1):
+            for j in range(1, self.height - 1):
+                if not self.obstacles[i, j]:
+                    div[i, j] = -0.5 * (velocX[i+1, j] - velocX[i-1, j] + velocY[i, j+1] - velocY[i, j-1]) / self.width
+                    p[i, j] = 0
+
+        self.set_bnd(0, div)
+        self.set_bnd(0, p)
+        self.lin_solve(0, p, div, 1, 4)
+
+        for i in range(1, self.width - 1):
+            for j in range(1, self.height - 1):
+                if not self.obstacles[i, j]:
+                    velocX[i, j] -= 0.5 * (p[i+1, j] - p[i-1, j]) * self.width
+                    velocY[i, j] -= 0.5 * (p[i, j+1] - p[i, j-1]) * self.height
+
+        self.set_bnd(1, velocX)
+        self.set_bnd(2, velocY)
+
+    def advect(self, b, d, d0, velocX, velocY):
+        dt0 = self.dt * self.width
+        for i in range(1, self.width - 1):
+            for j in range(1, self.height - 1):
+                if not self.obstacles[i, j]:
+                    tmp1 = dt0 * velocX[i, j]
+                    tmp2 = dt0 * velocY[i, j]
+                    x = i - tmp1
+                    y = j - tmp2
+                    if x < 0.5:
+                        x = 0.5
+                    if x > self.width + 0.5:
+                        x = self.width + 0.5
+                    i0 = int(x)
+                    i1 = i0 + 1
+                    if y < 0.5:
+                        y = 0.5
+                    if y > self.height + 0.5:
+                        y = self.height + 0.5
+                    j0 = int(y)
+                    j1 = j0 + 1
+                    s1 = x - i0
+                    s0 = 1 - s1
+                    t1 = y - j0
+                    t0 = 1 - t1
+                    try:
+                        d[i, j] = s0 * (t0 * d0[i0, j0] + t1 * d0[i0, j1]) + s1 * (t0 * d0[i1, j0] + t1 * d0[i1, j1])
+                    except: pass
+        self.set_bnd(b, d)
+
+    def set_bnd(self, b, x):
+        for i in range(1, self.width - 1):
+            for j in range(1, self.height - 1):
+                if self.obstacles[i, j]:
+                    x[i, j] = 0
+                else:
+                    if i == 1 or i == self.width - 2 or self.obstacles[i-1, j] or self.obstacles[i+1, j]:
+                        x[i, 0] = x[i, 1] if b == 2 else -x[i, 1]
+                        x[i, self.height - 1] = x[i, self.height - 2] if b == 2 else -x[i, self.height - 2]
+                    if j == 1 or j == self.height - 2 or self.obstacles[i, j-1] or self.obstacles[i, j+1]:
+                        x[0, j] = x[1, j] if b == 1 else -x[1, j]
+                        x[self.width - 1, j] = x[self.width - 2, j] if b == 1 else -x[self.width - 2, j]
+        x[0, 0] = 0.5 * (x[1, 0] + x[0, 1])
+        x[0, self.height - 1] = 0.5 * (x[1, self.height - 1] + x[0, self.height - 2])
+        x[self.width - 1, 0] = 0.5 * (x[self.width - 2, 0] + x[self.width - 1, 1])
+        x[self.width - 1, self.height - 1] = 0.5 * (x[self.width - 2, self.height - 1] + x[self.width - 1, self.height - 2])
---
+++ frame.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+import math
+import colorsys
+
+def process_frame(frame, scale):
+    resized_frame = cv2.resize(frame, (0, 0), fx=scale, fy=scale)
+
+    # Convertir l'image en niveaux de gris
+    gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+
+    # Appliquer un seuillage pour obtenir une image binaire
+    _, binary_frame = cv2.threshold(gray_frame, 128, 255, cv2.THRESH_BINARY)
+
+    # Convertir l'image binaire en tableau de 0 et 1
+    binary_array = (binary_frame // 255).astype(np.uint8)
+
+    return binary_array
+
+def get_binary_image_at_time(cap, t, scale):
+    # Définir la position de la vidéo à l'instant t (en secondes)
+    cap.set(cv2.CAP_PROP_POS_MSEC, t * 1000)
+
+    # Lire la frame à l'instant t
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Erreur lors de la lecture de la vidéo à l'instant t")
+        return None
+
+    # Traiter la frame
+    binary_array = process_frame(frame, scale)
+
+    return binary_array
+
+
+def dir2col(x, y):
+    length = math.sqrt(x*x + y*y)
+    if length == 0:
+        return (0, 0, 0)  # Handle zero length vector case
+
+    nx = x / length
+    ny = y / length
+
+    angle = math.atan2(ny, nx)
+
+    hue = angle * (180.0 / math.pi) + 90
+    if hue < 0:
+        hue += 360
+
+    c = 1.0
+    x_value = 1.0 - abs((hue / 60.0) % 2 - 1.0)
+    m = 0
+
+    if 0 <= hue < 60:
+        r_prime = c
+        g_prime = x_value
+        b_prime = 0
+    elif 60 <= hue < 120:
+        r_prime = x_value
+        g_prime = c
+        b_prime = 0
+    elif 120 <= hue < 180:
+        r_prime = 0
+        g_prime = c
+        b_prime = x_value
+    elif 180 <= hue < 240:
+        r_prime = 0
+        g_prime = x_value
+        b_prime = c
+    elif 240 <= hue < 300:
+        r_prime = x_value
+        g_prime = 0
+        b_prime = c
+    else:
+        r_prime = c
+        g_prime = 0
+        b_prime = x_value
+
+    r = int((r_prime + m) * 255)
+    g = int((g_prime + m) * 255)
+    b = int((b_prime + m) * 255)
+
+    return (r, g, b)
+
+def vel2col(x, y):
+    magnitude = math.sqrt(x**2 + y**2)
+    
+    normalized_magnitude = min(magnitude / 30, 1)  
+    
+    hue = normalized_magnitude  # Full range from red (0) to green (1)
+    saturation = 1.0
+    value = 1.0
+    
+    # Convert HSV to RGB
+    rgb = colorsys.hsv_to_rgb(hue, saturation, value)
+    rgb = tuple(int(c * 255) for c in rgb)
+    return rgb
